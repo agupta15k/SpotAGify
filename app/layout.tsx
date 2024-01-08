c8f7bb381427be17392d943911b5b67b371feffd
agupta57 <agupta57@ncsu.edu>
2024-01-08 15:18:02 -0500

    Add sidebar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,27 @@
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import './globals.css'
+import Sidebar from '@/components/Sidebar'
 
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Spotify Clone',
-  description: 'Listen to your favorite music, anytime, anywhere!!',
+	title: 'Spotify Clone',
+	description: 'Listen to your favorite music, anytime, anywhere!!',
 }
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={font.className}>
-        {children}
-      </body>
-    </html>
-  )
+  	return (
+		<html lang="en">
+			<body className={font.className}>
+				<Sidebar>
+					{children}
+				</Sidebar>
+			</body>
+		</html>
+  	)
 }
