2e801fddade94ba5fa118960683c56d0139fe6fb
agupta57 <agupta57@ncsu.edu>
2024-01-09 11:06:17 -0500

    Implement upload song modal

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,10 +1,20 @@
 "use client";
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
 
 const Library = () => {
+	const authModal = useAuthModal();
+	const uploadModal = useUploadModal();
+	const { user } = useUser();
 	const onClick = () => {
-		// Handle upload later
+		if (!user) {
+			return authModal.onOpen();
+		}
+		return uploadModal.onOpen();
+		// To do: Check for subscription
 	};
 
 	return (
